0643bf047b81719626ccd3960c01dda4f7d0b9d0
Christian Siedentop <40824622+seeedy@users.noreply.github.com>
2020-11-11 16:35:58 +0100

    testing react spring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 
+import { useSpring, animated } from 'react-spring';
+
 import '../styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const IndexPage = () => {
+    const props = useSpring({ number: 1000, from: { number: 0 } });
     return (
         <Layout>
             <h1>GAY</h1>
             <div>huhu</div>
+            <animated.div>{props.number}</animated.div>
         </Layout>
     );
 };
